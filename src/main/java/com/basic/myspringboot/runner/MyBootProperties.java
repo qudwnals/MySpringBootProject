a26f7342ffa954311bca2e7d7239cfaa7465d9f3
qudwnals <cwjk1008@naver.com>
2026-03-06 17:03:49 +0900

    외부 설정 및 프로퍼티 우선순위

---
+++ src/main/java/com/basic/myspringboot/runner/MyBootProperties.java
@@ -0,0 +1,18 @@
+package com.basic.myspringboot.runner;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("myboot")
+@Setter
+@Getter
+public class MyBootProperties {
+    private String name;
+    private int age;
+    private String fullName;
+
+}
---
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -0,0 +1,60 @@
+package com.basic.myspringboot.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private MyBootProperties properties;
+
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyBoorProperties getName() = " + properties.getName());
+        System.out.println("${myboot.name}  = " + name);
+        System.out.println("${myboot.age}  = " + age);
+
+        System.out.println("${myboot.fullName}  = " + environment.getProperty("myboot.fullName"));
+
+        System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
+        System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
+
+        /*
+            default void forEach(Consumer<? super T> action)
+            Consumer 인터페이스의 void accept(T t)
+         */
+        //Anonymous Inner Class
+        args.getOptionNames()
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
+
+        //Argument 목록 출력하기 (람다식)
+        args.getOptionNames()//Set<String>
+                .forEach(name -> System.out.println(name));
+
+        //Methoer Reference (메서드 레퍼런스)
+        args.getOptionNames()//Set<String>
+                .forEach(System.out::println);
+    }
+}
