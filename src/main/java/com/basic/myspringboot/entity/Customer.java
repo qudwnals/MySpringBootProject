e7c79bc3230a8d1825a18a40d5be18076fbe9387
qudwnals <cwjk1008@naver.com>
2026-03-09 15:21:33 +0900

    DB entity 생성

---
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -0,0 +1,18 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter @Setter
+public class Customer {
+    @Id @GeneratedValue(strategy =  GenerationType.AUTO)
+    private  Long id;
+
+
+
+
+
+}
