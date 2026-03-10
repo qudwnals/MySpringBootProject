ea2c3134f6f64c1ff36efb1e52ff06134b35d31f
qudwnals <cwjk1008@naver.com>
2026-03-10 16:01:02 +0900

    User Entity, Controller생성

---
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -0,0 +1,29 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
