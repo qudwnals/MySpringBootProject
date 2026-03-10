6c099d759199d29ef20a817bf50c9780597083c2
qudwnals <cwjk1008@naver.com>
2026-03-10 16:59:36 +0900

    User 등록, User 등록조회 컨트롤러 코드 추가

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,8 +1,36 @@
 package com.basic.myspringboot.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserRestController {
+    private final UserRepository userRepository;
 
+
+//    public UserRestController(UserRepository userRepository) {
+//        log.info("UserRepository 구현 클래스명 : {}", userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
+
+    //User 등록
+    @PostMapping
+    public User create(@RequestBody User userDetail){
+        return userRepository.save(userDetail);
+    }
+
+    //User 등록 조회
+    @GetMapping
+    public List<User> getUsers()
+    {
+        return userRepository.findAll();
+    }
 }
